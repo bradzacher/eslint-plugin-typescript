8c72e901d54bddf643921ac2fc62a6ddcb5820b8
roman <s91630277@gmail.com>
2018-03-09 18:49:41 +0400

    New: Add generic-type-naming (#111)

---
+++ tests/lib/rules/generic-type-naming.js
@@ -0,0 +1,64 @@
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/generic-type-naming"),
+    RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+    parser: "typescript-eslint-parser"
+});
+
+ruleTester.run("generic-type-naming", rule, {
+    valid: [
+        { code: "class<T,U,V> { }", options: [] },
+        { code: "type ReadOnly<T extends object> = {}", options: [] },
+        { code: "interface SimpleMap<V> { }", options: [] },
+        { code: "function get<T>() {}", options: [] },
+        { code: "interface GenericIdentityFn { <T>(arg: T): T }", options: [] },
+        { code: "class<x> { }", options: ["^x+$"] }
+    ],
+    invalid: [
+        {
+            code: "class<x> { }",
+            options: ["^[A-Z]+$"],
+            errors: [
+                { messageId: "nameRule", data: { name: "x", rule: "^[A-Z]+$" } }
+            ]
+        },
+        {
+            code: "interface SimpleMap<x> { }",
+            options: ["^[A-Z]+$"],
+            errors: [
+                { messageId: "nameRule", data: { name: "x", rule: "^[A-Z]+$" } }
+            ]
+        },
+        {
+            code: "type R<x> = {}",
+            options: ["^[A-Z]+$"],
+            errors: [
+                { messageId: "nameRule", data: { name: "x", rule: "^[A-Z]+$" } }
+            ]
+        },
+        {
+            code: "function get<x>() {}",
+            options: ["^[A-Z]+$"],
+            errors: [
+                { messageId: "nameRule", data: { name: "x", rule: "^[A-Z]+$" } }
+            ]
+        },
+        {
+            code: "interface GenericIdentityFn { <x>(arg: x): x }",
+            options: ["^[A-Z]+$"],
+            errors: [
+                { messageId: "nameRule", data: { name: "x", rule: "^[A-Z]+$" } }
+            ]
+        }
+    ]
+});
