f0c0f5810814e16ac6cca08ea4d3e7e7e7a20a4a
Brad Zacher <brad.zacher@gmail.com>
2018-12-11 21:14:25 -0800

    add support for class props, abstract class props, and abstract class decls

--- tests/lib/rules/indent.js
+++ tests/lib/rules/indent.js
@@ -25,6 +25,222 @@ ruleTester.run("indent", rule, {
     invalid: [
         {
             code: `
+type Foo = {
+bar : string,
+age : number,
+}
+            `,
+            output: `
+type Foo = {
+    bar : string,
+    age : number,
+}
+            `,
+            errors: [
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 3,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 4,
+                    column: 1,
+                },
+            ],
+        },
+        {
+            code: `
+interface Foo {
+bar : string,
+age : number,
+}
+            `,
+            output: `
+interface Foo {
+    bar : string,
+    age : number,
+}
+            `,
+            errors: [
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 3,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 4,
+                    column: 1,
+                },
+            ],
+        },
+        {
+            code: `
+interface Foo {
+bar : {
+baz : string,
+},
+age : number,
+}
+            `,
+            output: `
+interface Foo {
+    bar : {
+        baz : string,
+    },
+    age : number,
+}
+            `,
+            errors: [
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 3,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 8 spaces but found 0.`,
+                    line: 4,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 5,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 6,
+                    column: 1,
+                },
+            ],
+        },
+        {
+            code: `
+const foo : Foo<{
+bar : string,
+age : number,
+}>
+            `,
+            output: `
+const foo : Foo<{
+    bar : string,
+    age : number,
+}>
+            `,
+            errors: [
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 3,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 4,
+                    column: 1,
+                },
+            ],
+        },
+        {
+            code: `
+type T = {
+bar : string,
+age : number,
+} | {
+bar : string,
+age : number,
+}
+            `,
+            output: `
+type T = {
+    bar : string,
+    age : number,
+} | {
+    bar : string,
+    age : number,
+}
+            `,
+            errors: [
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 3,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 4,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 6,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 7,
+                    column: 1,
+                },
+            ],
+        },
+        {
+            code: `
+type T =
+    | {
+bar : string,
+age : number,
+}
+    | {
+    bar : string,
+    age : number,
+}
+            `,
+            output: `
+type T =
+    | {
+        bar : string,
+        age : number,
+    }
+    | {
+        bar : string,
+        age : number,
+    }
+            `,
+            errors: [
+                {
+                    message: `Expected indentation of 8 spaces but found 0.`,
+                    line: 4,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 8 spaces but found 0.`,
+                    line: 5,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 6,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 8 spaces but found 4.`,
+                    line: 8,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 8 spaces but found 4.`,
+                    line: 9,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 10,
+                    column: 1,
+                },
+            ],
+        },
+        {
+            code: `
     import Dialogs = require("widgets/Dialogs");
             `,
             output: `
@@ -34,6 +250,75 @@ import Dialogs = require("widgets/Dialogs");
                 {
                     message: `Expected indentation of 0 spaces but found 4.`,
                     line: 2,
+                    column: 1,
+                },
+            ],
+        },
+        {
+            code: `
+class Foo {
+public bar : string;
+private bar : string;
+protected bar : string;
+abstract bar : string;
+foo : string;
+}
+            `,
+            output: `
+class Foo {
+    public bar : string;
+    private bar : string;
+    protected bar : string;
+    abstract bar : string;
+    foo : string;
+}
+            `,
+            errors: [
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 3,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 4,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 5,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 6,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 4 spaces but found 0.`,
+                    line: 7,
+                    column: 1,
+                },
+            ],
+        },
+        {
+            code: `
+    abstract class Foo {}
+    class Foo {}
+            `,
+            output: `
+abstract class Foo {}
+class Foo {}
+            `,
+            errors: [
+                {
+                    message: `Expected indentation of 0 spaces but found 4.`,
+                    line: 2,
+                    column: 1,
+                },
+                {
+                    message: `Expected indentation of 0 spaces but found 4.`,
+                    line: 3,
                     column: 1,
                 },
             ],
