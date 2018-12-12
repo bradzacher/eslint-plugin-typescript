4af502130c6b21dee4709e41992976506906aeba
Brad Zacher <brad.zacher@gmail.com>
2018-12-11 22:48:20 -0800

    uncomment tests and added back the existing ones...

--- tests/lib/rules/indent.js
+++ tests/lib/rules/indent.js
@@ -21,9 +21,59 @@ const ruleTester = new RuleTester({
 });
 
 ruleTester.run("indent", rule, {
-    valid: [],
+    valid: [
+        `
+@Component({
+    components: {
+        ErrorPage: () => import('@/components/ErrorPage.vue'),
+    },
+    head: {
+        titleTemplate(title) {
+            if (title) {
+                return \`test\`
+            }
+            return 'Title'
+        },
+        htmlAttrs: {
+            lang: 'en',
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        ],
+    },
+})
+export default class App extends Vue
+{
+    get error()
+    {
+        return this.$store.state.errorHandler.error
+    }
+}
+        `,
+        // https://github.com/eslint/typescript-eslint-parser/issues/474
+        `
+/**
+ * @param {string} name
+ * @param {number} age
+ * @returns {string}
+ */
+function foo(name: string, age: number): string {}
+        `,
+        `
+const firebaseApp = firebase.apps.length
+    ? firebase.app()
+    : firebase.initializeApp({
+        apiKey: __FIREBASE_API_KEY__,
+        authDomain: __FIREBASE_AUTH_DOMAIN__,
+        databaseURL: __FIREBASE_DATABASE_URL__,
+        projectId: __FIREBASE_PROJECT_ID__,
+        storageBucket: __FIREBASE_STORAGE_BUCKET__,
+        messagingSenderId: __FIREBASE_MESSAGING_SENDER_ID__,
+    })
+        `,
+    ],
     invalid: [
-        /*
         {
             code: `
 type Foo = {
@@ -390,7 +440,6 @@ const enum Foo {
                 },
             ],
         },
-        */
         {
             code: `
     export = Foo;
