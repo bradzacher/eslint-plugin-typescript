735ec84bb44fc604f9d028974815b0e18ea7be34
Armano <armano2@users.noreply.github.com>
2018-12-18 00:00:55 +0100

    Fix merge conflict

--- tests/lib/rules/restrict-plus-operands.js
+++ tests/lib/rules/restrict-plus-operands.js
@@ -33,24 +33,12 @@ const ruleTester = new RuleTester({
 
 ruleTester.run("restrict-plus-operands", rule, {
     valid: [
-        {
-            code: `var x = 5;`,
-        },
-        {
-            code: `var y = "10";`,
-        },
-        {
-            code: `var z = 8.2;`,
-        },
-        {
-            code: `var w = "6.5";`,
-        },
-        {
-            code: `var foo = 5 + 10;`,
-        },
-        {
-            code: `var foo = "5.5" + "10";`,
-        },
+        `var x = 5;`,
+        `var y = "10";`,
+        `var z = 8.2;`,
+        `var w = "6.5";`,
+        `var foo = 5 + 10;`,
+        `var foo = "5.5" + "10";`,
         {
             filename: filePath,
             code: `var foo = parseInt("5.5", 10) + 10;`,
@@ -59,65 +47,41 @@ ruleTester.run("restrict-plus-operands", rule, {
             filename: filePath,
             code: `var foo = parseFloat("5.5", 10) + 10;`,
         },
-        {
-            code: `
+        `
 function test () : number { return 2; }
 var foo = test("5.5", 10) + 10;
             `,
-        },
-        {
-            code: `
+        `
 var x = 5;
 var z = 8.2;
 var foo = x + z;
             `,
-        },
-        {
-            code: `
+        `
 var w = "6.5";
 var y = "10";
 var foo = y + w;
             `,
-        },
-        {
-            code: "var foo = 1 + 1;",
-        },
-        {
-            code: "var foo = '1' + '1';",
-        },
-        {
-            code: `
+        "var foo = 1 + 1;",
+        "var foo = '1' + '1';",
+        `
 var pair: { first: number, second: string } = { first: 5, second: "10" };
 var foo = pair.first + 10;
             `,
-        },
-        {
-            code: `
+        `
 var pair: { first: number, second: string } = { first: 5, second: "10" };
 var foo = pair.first + (10 as number);
             `,
-        },
-        {
-            code: `
+        `
 var pair: { first: number, second: string } = { first: 5, second: "10" };
 var foo = "5.5" + pair.second;
             `,
-        },
-        {
-            code: `
+        `
 var pair: { first: number, second: string } = { first: 5, second: "10" };
 var foo = ("5.5" as string) + pair.second;
             `,
-        },
-        {
-            code: `const foo = 'hello' + (someBoolean ? 'a' : 'b') + (() => someBoolean ? 'c' : 'd')() + 'e';`,
-        },
-        {
-            code: `const balls = true;`,
-        },
-        {
-            code: `balls === true;`,
-        },
+        `const foo = 'hello' + (someBoolean ? 'a' : 'b') + (() => someBoolean ? 'c' : 'd')() + 'e';`,
+        `const balls = true;`,
+        `balls === true;`,
     ],
     invalid: [
         {
