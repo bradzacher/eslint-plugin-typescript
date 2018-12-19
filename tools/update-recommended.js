4da400949173c11ca285cdde99f3109cf260cb96
Brad Zacher <brad.zacher@gmail.com>
2018-12-19 10:16:26 -0800

    update recommended generator - turn off conflicting base rules - add additional config

--- tools/update-recommended.js
+++ tools/update-recommended.js
@@ -4,6 +4,8 @@ const path = require("path");
 const fs = require("fs");
 const requireIndex = require("requireindex");
 
+const bannedRecommendedRules = new Set(["camelcase", "indent"]);
+
 /**
  * Generate recommended configuration
  * @returns {void}
@@ -13,14 +15,31 @@ function generate() {
     const allRules = requireIndex(path.resolve(__dirname, "../lib/rules"));
     const rules = Object.keys(allRules)
         .filter(key => allRules[key].meta.docs.recommended)
-        .reduce((config, item) => {
-            config[item] = "error";
+        .reduce((config, key) => {
+            // having this here is just for output niceness (the keys will be ordered)
+            if (bannedRecommendedRules.has(key)) {
+                config[key] = "off";
+            }
+
+            config[`typescript/${key}`] = allRules[key].meta.docs.recommended;
             return config;
         }, {});
 
     const filePath = path.resolve(__dirname, "../lib/configs/recommended.json");
 
-    fs.writeFileSync(filePath, `${JSON.stringify({ rules }, null, 4)}\n`);
+    const recommendedConfig = {
+        parser: "eslint-plugin-typescript/parser",
+        parserOptions: {
+            sourceType: "module",
+        },
+        plugins: ["typescript"],
+        rules,
+    };
+
+    fs.writeFileSync(
+        filePath,
+        `${JSON.stringify(recommendedConfig, null, 4)}\n`
+    );
 }
 
 generate();
